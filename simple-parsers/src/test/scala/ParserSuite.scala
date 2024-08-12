e00a96865ddf50cfa0e511844fbd369f498c02da
Eirik Meland <eirik.meland@gmail.com>
2024-08-12 12:49:29 +0200

    parsere med capture

---
+++ simple-parsers/src/test/scala/ParserSuite.scala
@@ -0,0 +1,26 @@
+import cats.parse.{Parser, Rfc5234}
+import org.scalatest.funsuite.AnyFunSuite
+
+trait ParserSuite extends AnyFunSuite {
+
+  inline def assertParses[A](parser: Parser[A], inputs: (String, String)*) = {
+    inputs.foreach { (input, expectedResult) =>
+      val result = parser.parseAll(input)
+      assert(result === Right(expectedResult))
+    }
+  }
+
+  inline def assertParsesValid[A](parser: Parser[A], inputs: String*) = {
+    inputs.foreach { input =>
+      val result = parser.parseAll(input)
+      assert(result.isRight)
+    }
+  }
+
+  inline def assertParsesInvalid[A](parser: Parser[A], inputs: String*) = {
+    inputs.foreach { input =>
+      val result = parser.parseAll(input)
+      assert(result.isLeft)
+    }
+  }
+}
---
+++ simple-parsers/src/test/scala/ParsersWithCapture.scala
@@ -0,0 +1,30 @@
+import cats.parse.{Parser, Rfc5234}
+import org.scalatest.funsuite.AnyFunSuite
+
+class ParsersWithCapture extends ParserSuite {
+
+  test("parse \"a\" med vilkårlig mange spaces før og/eller etter") {
+    val validInputs = List(
+      "a"    -> "a",
+      " a"   -> "a",
+      "  a"  -> "a",
+      " a  " -> "a"
+    )
+
+    val p: Parser[String] = Parser.char('a').string.surroundedBy(Rfc5234.wsp.rep0)
+
+    assertParses(p, validInputs*)
+  }
+
+  test("character range") {
+    val validInputs = List(
+      "abcde" -> "abcde",
+      "ABCDE" -> "ABCDE",
+      "æøåÆØÅ" -> "æøåÆØÅ"
+    )
+
+    val p = Parser.charIn(0x00.toChar to 0x10ffff.toChar).rep.string
+
+    assertParses(p, validInputs*)
+  }
+}
