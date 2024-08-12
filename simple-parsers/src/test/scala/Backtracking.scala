9f5ceb69d7b1fbc483f9844849fad690ab070378
Eirik Meland <eirik.meland@gmail.com>
2024-08-12 13:48:33 +0200

    enda en parser med backtracking

--- simple-parsers/src/test/scala/Backtracking.scala
+++ simple-parsers/src/test/scala/Backtracking.scala
@@ -1,15 +1,37 @@
-import cats.parse.Parser
+import cats.parse.{Parser, Rfc5234}
 import org.scalatest.funsuite.AnyFunSuite
 
 class Backtracking extends ParserSuite {
 
-  enum Lengde {
-    case Millimeter(value: Int)
-    case Centimeter(value: Int)
-    case Meter(value: Int)
+  test("heltall eller flyttall") {
+    enum Tall {
+      case Flyttall(value: Double)
+      case Heltall(value: Int)
+    }
+
+    val validInputs = List(
+      "12"   -> Tall.Heltall(12),
+      "14.9" -> Tall.Flyttall(14.9),
+      ".55"  -> Tall.Flyttall(0.55)
+    )
+
+    val heltall = Rfc5234.digit.rep.string.map(s => Tall.Heltall(s.toInt)).withContext("heltall")
+    val flyttall = (Rfc5234.digit.rep0.with1 ~ Parser.char('.') ~ Rfc5234.digit.rep).string
+      .map(s => Tall.Flyttall(s.toDouble))
+      .withContext("flyttall")
+
+    val p = flyttall.backtrack | heltall
+
+    assertParses(p, validInputs*)
   }
 
   test("parse mm, cm eller m (f.eks. \"10cm\")") {
+    enum Lengde {
+      case Millimeter(value: Int)
+      case Centimeter(value: Int)
+      case Meter(value: Int)
+    }
+
     val validInputs = List(
       "10cm" -> Lengde.Centimeter(10),
       "100m" -> Lengde.Meter(100),
@@ -18,14 +40,17 @@ class Backtracking extends ParserSuite {
       "19m"  -> Lengde.Meter(19)
     )
 
-    val heltall: Parser[Int] = Parser.charsWhile(_.isDigit).map(_.toInt)
+    val heltall: Parser[Int] = Rfc5234.digit.rep.string.mapFilter(_.toIntOption)
 
-    val millimeter: Parser[Lengde] = (heltall <* Parser.string("mm")).map(Lengde.Millimeter.apply)
-    val centimeter: Parser[Lengde] = (heltall <* Parser.string("cm")).map(Lengde.Centimeter.apply)
-    val meter: Parser[Lengde]      = (heltall <* Parser.string("m")).map(Lengde.Meter.apply)
+    val millimeter: Parser[Lengde] =
+      (heltall <* Parser.string("mm")).map(Lengde.Millimeter.apply).withContext("millimeter")
+    val centimeter: Parser[Lengde] =
+      (heltall <* Parser.string("cm")).map(Lengde.Centimeter.apply).withContext("centimeter")
+    val meter: Parser[Lengde] = (heltall <* Parser.string("m")).map(Lengde.Meter.apply).withContext("meter")
 
     val p = millimeter.backtrack | centimeter.backtrack | meter
 
     assertParses(p, validInputs*)
   }
+
 }
