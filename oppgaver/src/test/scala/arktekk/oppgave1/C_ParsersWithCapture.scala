dbc78d46ea6ef9be94a641ab4080d1919cc699d0
Eirik Meland <eirik.meland@gmail.com>
2024-09-02 18:19:14 +0200

    bedre beskrivelser av oppgavene og noen hint

--- oppgaver/src/test/scala/arktekk/oppgave1/C_ParsersWithCapture.scala
+++ oppgaver/src/test/scala/arktekk/oppgave1/C_ParsersWithCapture.scala
@@ -4,8 +4,11 @@ import cats.parse.{Parser, Rfc5234}
 import arktekk.ParserSuite
 
 class C_ParsersWithCapture extends ParserSuite {
+  // her kommer vi til å få bruk for .string
 
   test("parse \"a\" med vilkårlig mange spaces før og/eller etter") {
+    // denne kan løses med .string og .surroundedBy
+    // eller med *>, <*, .with1 og .string
     val validInputs = List(
       "a"    -> "a",
       " a"   -> "a",
@@ -18,7 +21,8 @@ class C_ParsersWithCapture extends ParserSuite {
     assertParses(p, validInputs)
   }
 
-  test("character range") {
+  test("unicode characters") {
+    // alt fra 0x00 til 0x10ffff
     val validInputs = List(
       "abcde"  -> "abcde",
       "ABCDE"  -> "ABCDE",
@@ -31,6 +35,7 @@ class C_ParsersWithCapture extends ParserSuite {
   }
 
   test("boolean") {
+    // her trenger vi i tillegg .map eller .as
     val validInputs = List(
       "true"  -> true,
       "false" -> false
